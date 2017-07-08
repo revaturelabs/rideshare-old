d7f4822c8807d224add41bdc85508e5c4177b2e4
Chantel-Boor <chantel.boor@gmail.com>
2017-07-08 18:11:44 -0400

    view all rides done

--- rideshare/src/main/webapp/static/js/controllers/driver.controller.js
+++ rideshare/src/main/webapp/static/js/controllers/driver.controller.js
@@ -1,4 +1,13 @@
 export let driverController = function($scope, $http, $state){
-	$scope.test = 'driver home';
-
+	
+	//scope and fuction used to pass ride data to front end
+	
+	$scope.rides = {};
+	
+	  $http.get("/ride")
+	  .then(function(response) {
+	      $scope.rides = response.data;
+	      console.log($scope.rides);
+	  });
+	
 };
