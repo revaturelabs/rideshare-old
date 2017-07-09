48dea14b65724b2b7aa1ef21ea7c40745833bd41
Cody Boucher <Cody Boucher>
2017-07-09 15:54:58 -0400

    controllers wrote to add car, getting 403 error on post, needs token

--- rideshare/src/main/webapp/static/js/controllers/addCar.controller.js
+++ rideshare/src/main/webapp/static/js/controllers/addCar.controller.js
@@ -2,9 +2,6 @@ export let addCarController = function($scope, $http, $state){
 	$scope.car = {};
 	$scope.addCar = function() {
 		console.log($scope.car);
-		beforeSend: function(xhr) {
-			xhr.setRequestHeader("X-CSRF-TOKEN", token);
-		}
 		$http.post('/car', $scope.car).then(
 			(formResponse) => {
 				$state.go('success');
