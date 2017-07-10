a3e079c8709a12a05fd875527eb0543329a9d538
Cody Boucher <Cody Boucher>
2017-07-10 16:43:44 -0400

    set up admin ctrl class

--- rideshare/src/main/java/com/revature/rideshare/web/AdminController.java
+++ rideshare/src/main/java/com/revature/rideshare/web/AdminController.java
@@ -1,5 +1,6 @@
 package com.revature.rideshare.web;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.rideshare.domain.Car;
 import com.revature.rideshare.domain.Ride;
 import com.revature.rideshare.domain.User;
+import com.revature.rideshare.service.CarService;
 import com.revature.rideshare.service.RideService;
 import com.revature.rideshare.service.UserService;
 
@@ -21,5 +24,25 @@ public class AdminController {
 	
 	 @Autowired
 	 private UserService userService;
-	
+	 
+	 @Autowired
+	 private CarService carService;
+	 
+	 @GetMapping("/admin/cars")
+	     public List<Car> getAllCars(){
+		 	List<Car> cars = new ArrayList<Car>();
+		 	cars = carService.getAll();
+		 	System.out.println(cars);
+	        return carService.getAll();
+	 }
+	 
+	 @GetMapping("/admin/users")
+	    public List<User> getAllUsers(){
+	        return userService.getAll();
+	 }
+	 
+	 @GetMapping("/admin/rides")
+	 public List<Ride> getAllRides() {
+		 return rideService.getAll();
+	 }
 }
