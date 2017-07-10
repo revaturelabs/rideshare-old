c843ac45810a3d5503f811779d250e4e8d1488cc
Chantel-Boor <chantel.boor@gmail.com>
2017-07-10 12:52:14 -0400

    users can now see active and past rides

--- rideshare/src/main/webapp/static/js/controllers/driver.controller.js
+++ rideshare/src/main/webapp/static/js/controllers/driver.controller.js
@@ -1,12 +1,28 @@
 export let driverController = function($scope, $http, $state){
-	
+
 	// scope and function used to pass ride data to front end
-	
+
 	$scope.rides = {};
-	
+
 	$http.get("/ride")
-		.then(function(response) {
-			$scope.rides = response.data;
-		});
-	
+	.then(function(response) {
+		$scope.rides = response.data;
+	});
+
+	$scope.openRides = {};
+
+	$http.get("/ride/request/active")
+	.then(function(response){
+		$scope.openRides = response.data;
+	});
+
+	$scope.pastRides = {};
+
+	$http.get("/ride/request/history")
+	.then(function(response){
+		$scope.pastRides = response.data;
+	});
+
+
+
 };
