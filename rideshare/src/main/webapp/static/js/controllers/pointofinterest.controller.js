7a89ccaab9dee909892dc03d31a85f74cba1ad94
Theresa Dinh <tmdinh1201@gmail.com>
2017-07-11 15:48:59 -0400

    addpoi frontend linked to backend

--- rideshare/src/main/webapp/static/js/controllers/pointofinterest.controller.js
+++ rideshare/src/main/webapp/static/js/controllers/pointofinterest.controller.js
@@ -6,16 +6,19 @@ export let poiController = function ($scope, $http, $state) {
         console.log($scope.poi); //for debugging later
 
         // get address from form and format it for google maps response
-        var address = "" + $scope.poiAddressLine1 +
-            $scope.poiAddressLine2 + ", " + $scope.poiCity +
-            ", " + $scope.poiState;
-        address.replace(/\s+/g, '+'); //replace white space with +
+        var address = "" + $scope.poi.poiAddressLine1.toString() + " "
+            $scope.poi.poiAddressLine2.toString() + ", " + $scope.poi.poiCity.toString() +
+            ", " + $scope.poi.poiState.toString();
+        address = address.replace(/\s+/g, '+'); //replace white space with +
+        console.log("address: " + address); // debugging 
 
         // retrieve the latitude and longitude from the form response
         var url = "https://maps.googleapis.com/maps/api/geocode/" +
             "json?address=" + address +
             "&key=AIzaSyB_mhIdxsdRfwiAHVm8qPufCklQ0iMOt6A";
+        console.log("url " + url); // debugging
         
+
         $http.get(url)
             .then(function (response) {
                 $scope.latitude = response.results[0].geometry.location.lat;
