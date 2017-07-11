f6a56459e219234efc922bdc1bf3ede1c7be58c5
Cody Boucher <Cody Boucher>
2017-07-11 11:00:02 -0400

    work on admin ctrl

--- rideshare/src/main/java/com/revature/rideshare/web/AdminController.java
+++ rideshare/src/main/java/com/revature/rideshare/web/AdminController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,6 +41,8 @@ public class AdminController {
 	    public List<User> getAllUsers(){
 	        return userService.getAll();
 	 }
+	 
+	 
 	 
 	 @GetMapping("/admin/rides")
 	 public List<Ride> getAllRides() {
