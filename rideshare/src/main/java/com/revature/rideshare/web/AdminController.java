70dd77b0b6970418ed5fc5252e0c77d900e210ec
Cody Boucher <Cody Boucher>
2017-07-11 15:39:48 -0400

    java controller built, waiting for methods in ride service

--- rideshare/src/main/java/com/revature/rideshare/web/AdminController.java
+++ rideshare/src/main/java/com/revature/rideshare/web/AdminController.java
@@ -5,14 +5,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.rideshare.domain.Car;
+import com.revature.rideshare.domain.PointOfInterest;
 import com.revature.rideshare.domain.Ride;
 import com.revature.rideshare.domain.User;
 import com.revature.rideshare.service.CarService;
+import com.revature.rideshare.service.PointOfInterestService;
 import com.revature.rideshare.service.RideService;
 import com.revature.rideshare.service.UserService;
 
@@ -21,31 +25,49 @@ import com.revature.rideshare.service.UserService;
 public class AdminController {
 
 	@Autowired
-	private RideService rideService;
+	private RideService rideService; 
 	
-	 @Autowired
-	 private UserService userService;
+	@Autowired
+	private UserService userService;
 	 
-	 @Autowired
-	 private CarService carService;
+	@Autowired
+	private CarService carService;
 	 
-	 @GetMapping("/admin/cars")
-	     public List<Car> getAllCars(){
+	@Autowired
+	private PointOfInterestService poiService;
+	 
+	@GetMapping("/admin/cars")
+	    public List<Car> getAllCars(){
 		 	List<Car> cars = new ArrayList<Car>();
 		 	cars = carService.getAll();
 		 	System.out.println(cars);
-	        return carService.getAll();
-	 }
+		 	return carService.getAll();
+	}
 	 
-	 @GetMapping("/admin/users")
-	    public List<User> getAllUsers(){
-	        return userService.getAll();
-	 }
+	@GetMapping("/admin/users")
+		public List<User> getAllUsers(){
+			return userService.getAll();
+	}
 	 
+	@PostMapping("/admin/updateStatus/{id}") 
+		public void updateStatus(@PathVariable(value="id") long id) {
+			User user = userService.getUser(id);
+			userService.updateUser(user);
+	}
 	 
+	@PostMapping("/admin/removeUser/{id}")
+	public void removeUser(@PathVariable(value="id") long id) {
+		User user = userService.getUser(id);
+		userService.removeUser(user);
+	}
 	 
-	 @GetMapping("/admin/rides")
-	 public List<Ride> getAllRides() {
-		 return rideService.getAll();
-	 }
+	@PostMapping("/admin/addPOI")
+	public void addPoi(@RequestBody PointOfInterest poi){
+		poiService.addPoi(poi);
+	}
+	 
+	@PostMapping("/admin/removePOI")
+	public void removePoi(@RequestBody PointOfInterest poi){
+		poiService.removePoi(poi);
+	}
 }
