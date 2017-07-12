7606f932942827959fefe47c9529d324171d0590
Theresa Dinh <tmdinh1201@gmail.com>
2017-07-12 10:11:42 -0400

    cursed line endings

--- rideshare/src/main/webapp/static/js/controllers/pointofinterest.controller.js
+++ rideshare/src/main/webapp/static/js/controllers/pointofinterest.controller.js
@@ -6,10 +6,12 @@ export let poiController = function ($scope, $http, $state) {
         console.log($scope.poi); //for debugging later
 
         // get address from form and format it for google maps response
-        var address = "" + $scope.poi.poiAddressLine1.toString() + " "
-            $scope.poi.poiAddressLine2.toString() + ", " + $scope.poi.poiCity.toString() +
-            ", " + $scope.poi.poiState.toString();
-        address = address.replace(/\s+/g, '+'); //replace white space with +
+        var address = "" + $scope.poi.poiAddressLine1 + " "
+            + $scope.poi.poiAddressLine2 + ", " +
+            $scope.poi.poiCity +
+            ", " + $scope.poi.poiState;
+        // console.log(typeof address);
+        address = address.replace(/\s/g, '+'); // replace white space with +
         console.log("address: " + address); // debugging 
 
         // retrieve the latitude and longitude from the form response
@@ -17,18 +19,18 @@ export let poiController = function ($scope, $http, $state) {
             "json?address=" + address +
             "&key=AIzaSyB_mhIdxsdRfwiAHVm8qPufCklQ0iMOt6A";
         console.log("url " + url); // debugging
-        
+
 
         $http.get(url)
             .then(function (response) {
-                $scope.latitude = response.results[0].geometry.location.lat;
-                $scope.longitude = response.results[0].geometry.location.lng;
+                $scope.poi.latitude = response.results[0].geometry.location.lat;
+                $scope.poi.longitude = response.results[0].geometry.location.lng;
             });
 
         // add poi
-        $http.post("/poi", $scope.poi)
+        $http.post("/poiController/addPoi", $scope.poi)
             .then((formResponse) => {
-                $state.go('poi');   
+                $state.go('poi');
             },
             (failedResponse) => {
                 alert('failure');
