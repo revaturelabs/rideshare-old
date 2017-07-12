a2b8324170a3a1866c7265cf43af5a98ea8a225b
Theresa Dinh <tmdinh1201@gmail.com>
2017-07-12 14:02:34 -0400

    able to retrieve lat and lng from google api response

--- rideshare/src/main/webapp/static/js/controllers/pointofinterest.controller.js
+++ rideshare/src/main/webapp/static/js/controllers/pointofinterest.controller.js
@@ -5,26 +5,32 @@ export let poiController = function ($scope, $http, $state) {
     $scope.addPoi = function () {
         console.log($scope.poi); //for debugging later
 
+        if($scope.poi.addressLine2 === undefined)
+            $scope.poi.addressLine2 = "";
         // get address from form and format it for google maps response
-        var address = "" + $scope.poi.poiAddressLine1 + " "
-            + $scope.poi.poiAddressLine2 + ", " +
-            $scope.poi.poiCity +
-            ", " + $scope.poi.poiState;
-        // console.log(typeof address);
+        var address = "" + $scope.poi.addressLine1 + " "
+            + $scope.poi.addressLine2 + ", " +
+            $scope.poi.city +
+            ", " + $scope.poi.state;
+        // console.log(type of address);
         address = address.replace(/\s/g, '+'); // replace white space with +
-        console.log("address: " + address); // debugging 
+        // console.log("address: " + address); // debugging 
 
         // retrieve the latitude and longitude from the form response
         var url = "https://maps.googleapis.com/maps/api/geocode/" +
             "json?address=" + address +
             "&key=AIzaSyB_mhIdxsdRfwiAHVm8qPufCklQ0iMOt6A";
-        console.log("url " + url); // debugging
+        // console.log("url " + url); // debugging
 
+        var jsonresponse = []; 
 
         $http.get(url)
             .then(function (response) {
-                $scope.poi.latitude = response.results[0].geometry.location.lat;
-                $scope.poi.longitude = response.results[0].geometry.location.lng;
+                console.log("sanity check #" + 2);
+
+                $scope.result = response; 
+                $scope.poi.latitude = $scope.result.data.results[0].geometry.location.lat;
+                $scope.poi.longitude = $scope.result.data.results[0].geometry.location.lng;
             });
 
         // add poi
