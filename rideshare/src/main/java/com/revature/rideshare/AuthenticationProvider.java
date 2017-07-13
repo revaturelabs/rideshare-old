997186f42bce49a20cd9e334f60ab9b0b97418cb
mrworth <markworth89@gmail.com>
2017-07-13 11:07:28 -0400

    updating with rides formate

---
+++ rideshare/src/main/java/com/revature/rideshare/AuthenticationProvider.java
@@ -0,0 +1,10 @@
+//package com.revature.rideshare;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//
+//public class AuthenticationProvider {
+//
+//	@Bean
+//	public AuthenticationProvider authenticationProvider;
+//}
