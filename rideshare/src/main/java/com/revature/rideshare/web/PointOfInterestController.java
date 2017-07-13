9ea9a82f84eef493b04773e26f1bd63a337ed892
Theresa Dinh <tmdinh1201@gmail.com>
2017-07-13 11:40:24 -0400

    addPoi form able to pass poi type as object now and other minor fixes

--- rideshare/src/main/java/com/revature/rideshare/web/PointOfInterestController.java
+++ rideshare/src/main/java/com/revature/rideshare/web/PointOfInterestController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.rideshare.domain.PointOfInterest;
+import com.revature.rideshare.domain.PointOfInterestType;
 import com.revature.rideshare.service.PointOfInterestService;
 
 @RestController
@@ -24,8 +26,16 @@ public class PointOfInterestController {
         return poiService.getAll();
     }
 
+    @GetMapping("/type")
+    public List<PointOfInterestType> getAllTypes(){
+        return poiService.getAllTypes();
+    }
+
     @PostMapping("/addPoi")
-    public void addPoi(@RequestBody PointOfInterest poi){
+    public void addPoi(@RequestBody String jsonPoi){
+        System.out.println("in addPoi");
+        PointOfInterest poi = getPoi(jsonPoi);
+        System.out.println("\n\n\n" + poi.toString() + "\n\n\n");
         poiService.addPoi(poi);
     }
 
@@ -37,5 +47,18 @@ public class PointOfInterestController {
     @PostMapping("/updatePoi")
     public void updatePoi(@RequestBody PointOfInterest poi){
         poiService.updatePoi(poi);
+    }
+
+    public PointOfInterest getPoi(String jsonString){
+        ObjectMapper mapper = new ObjectMapper();
+        
+        System.out.println("in getPoi \n\n\n" + jsonString + "\n\n\n\n"); 
+        try{
+                return (PointOfInterest)mapper.readValue(jsonString, PointOfInterest.class);            
+        } catch(Exception e){
+            System.out.println("\n\n\n exception returns null \n\n\n");
+            e.printStackTrace();
+                return null; 
+        }
     }
 }
