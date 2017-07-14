6579fea4cf9436f0db679a5aa67889347bb0232b
Eric <ericjchristie@gmail.com>
2017-07-14 01:18:06 -0400

    committing changes before pulling

---
+++ rideshare/src/main/java/com/revature/rideshare/RideshareAuthenticationManager.java
@@ -0,0 +1,15 @@
+package com.revature.rideshare;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+public class RideshareAuthenticationManager implements AuthenticationManager {
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
---
+++ rideshare/src/main/java/com/revature/rideshare/service/AuthServiceImpl.java
@@ -0,0 +1,8 @@
+package com.revature.rideshare.service;
+
+import org.springframework.stereotype.Component;
+
+//@Component("authService")
+public class AuthServiceImpl {
+
+}
