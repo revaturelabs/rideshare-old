7295a9dc1a27480ad58b8d77af673b00743e96e0
James Champ <jameschamp@acm.org>
2017-07-16 07:50:30 -0400

    Users can have only one car
    Client side fix for the many car issue. If a user does not have a
    vehicle in the system, they are given the option to add one. If they do
    have a car in the system, they are given the option to edit or remove 
    their vehicle.

--- rideshare/src/main/webapp/static/js/controllers/userProfile.controller.js
+++ rideshare/src/main/webapp/static/js/controllers/userProfile.controller.js
@@ -1,49 +1,58 @@
 export let userProfileController = function ($scope, $http, $state){
-    $scope.allpois = {}; 
-    $scope.user = {}; 
-    $scope.car = {};
-        
-    //retrieve all pois
-    $http.get("/poiController")
-    .then(function (response){
-        $scope.allpois = response.data; 
-    });
-    
-    // retrieve the user's current car
+	$scope.allpois = {}; 
+	$scope.user = {}; 
+	$scope.car = {};
+	$scope.buttonText = '';
+	//retrieve all pois
+	$http.get("/poiController")
+	.then(function (response){
+		$scope.allpois = response.data; 
+	});
+	
+	// retrieve the user's current car
 	$http.get("/car/myCar", $scope.car)
 		.then((response) => {
-			$scope.car = response.data; 
+			$scope.car = response.data;
+			
+			if($scope.car === '') {
+				$scope.buttonText = 'Add Car';
+			}
+			else {
+				$scope.buttonText = 'Edit Car';
+			}
 		},
 		(failedResponse) => {
 			alert('failure'); 
 		})
-    
-    // how get poi from selected option 
-    // set the pois to the user 
-    $scope.setPois = function(){  
-        $http.post("/user/updateCurrentUser", $scope.user)
-        .then((formResponse) => {
-            $state.go('main.userProfile');
-        },
-        (failedResponse) => {
-            alert('failure');
-        })
-    }
-    
+	
+	// how get poi from selected option 
+	// set the pois to the user 
+	$scope.setPois = function(){  
+		$http.post("/user/updateCurrentUser", $scope.user)
+		.then((formResponse) => {
+			$state.go('main.userProfile');
+		},
+		(failedResponse) => {
+			alert('failure');
+		})
+	}
+	
 	$scope.addCar = function() {
 		$http.post('/car', $scope.car).then(
-            (formResponse) => {
-                $state.go('main.userProfile');
-            },
-            (failedResponse) => {
-                alert('Failure');
-            }
+			(formResponse) => {
+				$scope.buttonText = 'Edit Car';
+			    $state.go('main.userProfile');
+			},
+			(failedResponse) => {
+			    alert('Failure');
+			}
 		)
 	}
 	
 	$scope.removeCar = function(){
 		$http.post("/car/removeCar", $scope.car)
 			.then((response) => {
+				$scope.buttonText = 'Add Car';
 				$state.go("main.userProfile");
 			},
 			(failedResponse) => {
