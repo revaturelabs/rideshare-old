224af3af79c1bb965b8f4d16e0517a668a4a52c9
Eric <ericjchristie@gmail.com>
2017-07-17 00:59:21 -0400

    moved some files to com.revature.rideshare.security

--- rideshare/src/main/java/com/revature/rideshare/security/JwtAuthenticationProvider.java
+++ rideshare/src/main/java/com/revature/rideshare/security/JwtAuthenticationProvider.java
@@ -1,4 +1,4 @@
-package com.revature.rideshare;
+package com.revature.rideshare.security;
 
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
