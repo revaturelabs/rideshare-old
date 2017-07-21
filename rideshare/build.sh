d8a220eea2b6098e7ec6c11cb3248cfe0c4c2d88
Eric Christie <ejchristie@users.noreply.github.com>
2017-07-21 09:59:55 -0400

    Update build.sh

--- rideshare/build.sh
+++ rideshare/build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 # This is a shell script for executing the entired build process for the Revature RideShare project.
 
-# Get the latest version of npm
-npm i -g npm
 # Install all JavaScript dependencies
 npm Install
 # Run webpack
