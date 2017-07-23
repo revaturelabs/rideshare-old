7d7db6ad1dd477bf87f83d9c66a951a0fc468753
Eric <ericjchristie@gmail.com>
2017-07-23 11:05:21 -0400

    removed unused code

--- rideshare/src/main/webapp/static/js/controllers/slackLogin.controller.js
+++ rideshare/src/main/webapp/static/js/controllers/slackLogin.controller.js
@@ -5,32 +5,4 @@ export let slackLoginController = function($scope, $http, $state) {
 			localStorage.setItem('RideShare_auth_token', token);
 			$state.go('main.passenger');
 		});
-
-	// $scope.checkAuth = function() {
-	// 	$http.get("/auth/check")
-	// 	.then(function(res) {
-	// 		console.log(res);
-	// 		if (res.data !== null && res.data === true) {
-	// 			$scope.authenticated = true;
-	// 			$state.go('main.passenger');
-	// 		} else {
-	// 			$scope.authenticated = false;
-	// 		}
-	// 	})
-	// 	.catch(function() {
-	// 		$scope.authenticated = false;
-	// 	});
-	// }
-	// $scope.checkAuth();
-	
-
-
-	// $http.get('/auth/token')
-	// .then(function(res) {
-	// 	// put res (token) in localstorage
-	// 	localStorage.setItem('RideShare_auth_token', res.headers('rideshare-token'));
-	// 	if ($rootScope.isAuthenticated) {
-	// 		$state.go('main.passenger');
-	// 	}
-	// });
 }
