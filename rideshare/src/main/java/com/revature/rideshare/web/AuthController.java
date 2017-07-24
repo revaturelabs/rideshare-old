691407923c36d3740dd1575218f25d257dc8841b
Eric <ericjchristie@gmail.com>
2017-07-23 20:02:55 -0400

    committing changes

--- rideshare/src/main/java/com/revature/rideshare/web/AuthController.java
+++ rideshare/src/main/java/com/revature/rideshare/web/AuthController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
