a40996fa1de081badfd7882fef4d9123d294d9b1
Eric <ericjchristie@gmail.com>
2017-08-06 13:28:11 -0400

    moved ClientResource into its own class

---
+++ rideshare/src/main/java/com/revature/rideshare/security/ClientResource.java
@@ -0,0 +1,21 @@
+package com.revature.rideshare.security;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+
+public class ClientResource {
+	@NestedConfigurationProperty
+	private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
+	
+	@NestedConfigurationProperty
+	private ResourceServerProperties resource = new ResourceServerProperties();
+
+	public AuthorizationCodeResourceDetails getClient() {
+		return client;
+	}
+
+	public ResourceServerProperties getResource() {
+		return resource;
+	}
+}
