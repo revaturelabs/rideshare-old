5b583d0920f53d3a3507118dfdb677e970edb15f
Eric <ericjchristie@gmail.com>
2017-08-13 14:15:43 -0400

    cleaned up custom principal extractor

--- rideshare/src/main/java/com/revature/rideshare/security/RidesharePrincipalExtractor.java
+++ rideshare/src/main/java/com/revature/rideshare/security/RidesharePrincipalExtractor.java
@@ -1,7 +1,7 @@
 package com.revature.rideshare.security;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 
@@ -11,19 +11,13 @@ public class RidesharePrincipalExtractor implements PrincipalExtractor {
 	 * A principal will identify a user by Slack Id.
 	 * Email could also be used to identify users if the Slack integration needs to work with multiple teams.
 	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public Object extractPrincipal(Map<String, Object> map) {
 		String principal = null;
-		System.out.println("in principal extractor");
-		if (new Boolean((String) map.get("ok"))) {
-			for (Entry<String, Object> e: map.entrySet()) {
-				System.out.println(e.getKey() + " = " + e.getValue());
-			}
-			String[] parts = ((String) map.get("user")).split(", ");
-			String fullname = parts[0].substring(6);
-			String slackId = parts[1].substring(3);
-			String email = parts[2].substring(6, parts[2].length() - 1);
-			System.out.println("name = " + fullname + ", slackId = " + slackId + ", email = " + email);
+		if ((Boolean) map.get("ok")) {
+			String slackId = ((LinkedHashMap<String, String>) map.get("user")).get("id");
+//			String email = ((LinkedHashMap<String, String>) map.get("user")).get("email");
 			principal = slackId;
 		}
 		return principal;
